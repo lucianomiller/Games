b6dff7a78aaa9a27e95c10011cae34db0b08c14e
lucianomiller <37517895+lucianomiller@users.noreply.github.com>
2021-04-07 22:38:40 -0300

    agrego page of games

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./styles.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, Link } from "react";
+import { Switch, Route } from "react-router-dom";
 import axios from "axios";
 import Card from "./Card";
+import { useHistory, useParams } from "react-router-dom";
 const options = {
   method: "GET",
   url:
@@ -15,12 +17,13 @@ const options = {
 
 export default function App() {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     axios
       .request(options)
       .then(function (response) {
         setData(response.data.results);
-        //console.log(response.data);
+        console.log(response.data);
       })
       .catch(function (error) {
         //console.error(error);
@@ -29,44 +32,233 @@ export default function App() {
   const [searchVal, setSearchVal] = useState("");
   const search = (e) => {
     e.preventDefault();
-    console.log(searchVal);
+    //console.log(searchVal);
     axios
       .get(`https://api.rawg.io/api/games?search=${searchVal}`)
 
       .then(function (response) {
         setData(response.data.results);
+
         //console.log(response.data);
-      });
+      })
+      .then(() => setSearchVal(""));
   };
   return (
-    <div className="">
-      <div>
-        <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
-          <div class="container-fluid">
-            <a class="navbar-brand">Games</a>
-            <form class="d-flex" onSubmit={search}>
-              <input
-                onChange={(e) => setSearchVal(e.target.value)}
-                class="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button class="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </nav>
-      </div>
+    <>
+      <Switch>
+        <div className="">
+          <Route path="/">
+            <div>
+              <nav class="navbar navbar-expand-xl navbar-dark bg-dark">
+                <div class="container-fluid">
+                  <a href="/" class="navbar-brand">
+                    Games
+                  </a>
+                  <form class="d-flex" onSubmit={search}>
+                    <input
+                      onChange={(e) => setSearchVal(e.target.value)}
+                      class="form-control me-2"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                      value={searchVal}
+                    />
+                    <button class="btn btn-outline-success" type="submit">
+                      Search
+                    </button>
+                  </form>
+                </div>
+              </nav>
+            </div>
+          </Route>
+          <Route exact path="/">
+            <div className=" page-content App">
+              {data &&
+                data.map((arr) => {
+                  return (
+                    <Card
+                      name={arr.name}
+                      background_image={arr.background_image}
+                      released={arr.released}
+                      id={arr.id}
+                    />
+                  );
+                })}
+            </div>
+          </Route>
 
-      <div className=" page-content App">
-        {data &&
-          data.map((arr) => {
-            return (
-              <Card name={arr.name} background_image={arr.background_image} />
-            );
-          })}
+          <Route path="/:id" children={<Details />} />
+        </div>
+      </Switch>
+    </>
+  );
+}
+
+import "./main.css";
+function Details() {
+  const [data, setData] = useState([]);
+
+  let { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://api.rawg.io/api/games/${id}`)
+      .then(function (response) {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        //console.error(error);
+      });
+  }, []);
+
+  return (
+    <div class="is-preload">
+      <header id="header">
+        <img src={data.background_image} alt="" />
+        <h1>
+          <strong>{data.name}</strong> <br />
+          <br />
+          <a href={data.website}>Oficial Page</a>.
+        </h1>
+        {/* <div class="inner">
+      <a href="#" class="image avatar"><img src={data.background_image} alt="" /></a>
+      <h1><strong>I am Strata</strong>, a super simple<br />
+      responsive site template freebie<br />
+      crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
+    </div> */}
+      </header>
+
+      <div id="main">
+        <section id="one">
+          <header class="major">
+            <h2>
+              Ipsum lorem dolor aliquam ante commodo
+              <br />
+              magna sed accumsan arcu neque.
+            </h2>
+          </header>
+          <p>{data.description_raw}</p>
+          <ul class="actions">
+            <li>
+              <a href="#" class="button">
+                Learn More
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        <section id="two">
+          <h2>Recent Work</h2>
+          <div class="row">
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/01.jpg" class="image fit thumb">
+                <img src="images/thumbs/01.jpg" alt="" />
+              </a>
+              <h3>Magna sed consequat tempus</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/02.jpg" class="image fit thumb">
+                <img src="images/thumbs/02.jpg" alt="" />
+              </a>
+              <h3>Ultricies lacinia interdum</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/03.jpg" class="image fit thumb">
+                <img src="images/thumbs/03.jpg" alt="" />
+              </a>
+              <h3>Tortor metus commodo</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/04.jpg" class="image fit thumb">
+                <img src="images/thumbs/04.jpg" alt="" />
+              </a>
+              <h3>Quam neque phasellus</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/05.jpg" class="image fit thumb">
+                <img src="images/thumbs/05.jpg" alt="" />
+              </a>
+              <h3>Nunc enim commodo aliquet</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+            <article class="col-6 col-12-xsmall work-item">
+              <a href="images/fulls/06.jpg" class="image fit thumb">
+                <img src="images/thumbs/06.jpg" alt="" />
+              </a>
+              <h3>Risus ornare lacinia</h3>
+              <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+            </article>
+          </div>
+          <ul class="actions">
+            <li>
+              <a href="#" class="button">
+                Full Portfolio
+              </a>
+            </li>
+          </ul>
+        </section>
+
+        {/* <section id="three">
+        <h2>Get In Touch</h2>
+        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+        <div class="row">
+          <div class="col-8 col-12-small">
+            <form method="post" action="#">
+              <div class="row gtr-uniform gtr-50">
+                <div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                <div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                <div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
+              </div>
+            </form>
+            <ul class="actions">
+              <li><input type="submit" value="Send Message" /></li>
+            </ul>
+          </div>
+          <div class="col-4 col-12-small">
+            <ul class="labeled-icons">
+              <li>
+                <h3 class="icon solid fa-home"><span class="label">Address</span></h3>
+                1234 Somewhere Rd.<br />
+                Nashville, TN 00000<br />
+                United States
+              </li>
+              <li>
+                <h3 class="icon solid fa-mobile-alt"><span class="label">Phone</span></h3>
+                000-000-0000
+              </li>
+              <li>
+                <h3 class="icon solid fa-envelope"><span class="label">Email</span></h3>
+                <a href="#">hello@untitled.tld</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section> */}
+        {/* <footer id="footer">
+				<div class="inner">
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer> */}
+
+        {/* <script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.poptrox.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script> */}
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,6 +1,7 @@
 import "./card.css";
-export default function Card({ name, background_image }) {
-  console.log(name);
+import { Link } from "react-router-dom";
+export default function Card({ name, background_image, released, id }) {
+  //console.log(name);
   return (
     <div class="">
       <div class="news-card">
@@ -8,16 +9,18 @@ export default function Card({ name, background_image }) {
         <img src={background_image} alt="" class="news-card__image" />
         <div class="news-card__text-wrapper">
           <h2 class="news-card__title">{name}</h2>
-          <div class="news-card__post-date">Jan 29, 2018</div>
+          <div class="news-card__post-date">{released}</div>
           <div class="news-card__details-wrapper">
-            <p class="news-card__excerpt">
+            {/* <p class="news-card__excerpt">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
               pariatur nemo tempore repellat? Ullam sed officia iure architecto
               deserunt distinctio, pariatur&hellip;
-            </p>
-            <a href="wwww.google.com" class="news-card__read-more">
-              Read more <i class="fas fa-long-arrow-alt-right"></i>
-            </a>
+            </p> */}
+            <Link to={"/" + id}>
+              <a /* href={"/" + id} */ class="news-card__read-more">
+                Read more <i class="fas fa-long-arrow-alt-right"></i>
+              </a>
+            </Link>
           </div>
         </div>
       </div>
